b8448755acf5f8325ef1a2718c97e5b19b1963ed
JeroenAdO <jeroen.arrazoladeonate@gmail.com>
2021-08-06 16:18:40 +0200

    First commit

---
+++ siemens-to-prometheus/Services/ConfigService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace siemens_to_prometheus.Services
+{
+    public class ConfigService
+    {
+        public 
+    }
+}
